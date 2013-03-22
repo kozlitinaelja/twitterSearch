0e487f94a916fb31bd957cd5b9cc3f9dda5df56a
kozlitinaelja <kozlitinaelja@gmail.com>
2013-03-21 22:37:08 -0400

    refactored twittsAnimation method

--- js/twitterSearch.js
+++ js/twitterSearch.js
@@ -20,15 +20,17 @@
       searchTemplate: "searchTemplate",
       twittTagName: "li",
       twittsTagName: "ul",
-      timeOut: 0,
+      searchTimeout: 0,
+      animationTimeout: 0,
       searchDelay: 1000,
-      search: "monkey",
+      updateDelay: 20000,
+      animationSpeed: 600,
+      animationDelay: 900,
+      search: "USA",
       rpp: 30,
       result_type: 'recent',
-      updateSpeed: 20000,
-      timedDelay: 0,
-      searchBar: true,
-      fadeEffect: true,
+      searchBar: false,
+      fadeEffect: false,
       current: 0
 
     };
@@ -49,14 +51,14 @@
       difDay = Math.floor(dif / 86400);
 
       if (isNaN(difDay) || difDay < 0 || difDay > 31) {
-        return
+        return;
       }
 
       time = (difDay === 0) ? (dif < 60) && 'just now' ||
           (dif < 120) && '1 min ago' ||
           (dif < 3600) && (Math.floor(dif / 60) + ' min ago') ||
           (dif < 7200) && 'an hour ago' ||
-          (dif < 86400) && (Math.floor(dif / (60 * 60)) + ' hours ago') :
+          (dif < 86400) && (Math.floor(dif / 3600) + ' hours ago') :
           (difDay === 1) && 'one day ago' ||
               (difDay < 7) && difDay + ' days ago' ||
               (difDay < 14) && 'a week ago' ||
@@ -83,7 +85,6 @@
       model: Twitter.Models.Twitt,
       url: "http://search.twitter.com/search.json",
       parse: function(response) {
-        console.log(response.results);
         $.each(response.results, this.formatTwitts);
         return  response.results;
       },
@@ -112,10 +113,10 @@
           dataType: "jsonp",
 
           success: function() {
-            clearTimeout(config.timedDelay);
+            clearTimeout(config.animationTimeout);
             setTimeout(function() {
               self.updateResults()
-            }, config.updateSpeed);
+            }, config.updateDelay);
           }});
       }
 
@@ -140,50 +141,58 @@
       render: function() {
         var self = this;
         this.addAll();
-        setTimeout(function() {self.twittsAnimation()}, 600);
+        setTimeout(function() {
+          self.twittsAnimation(0)
+        }, 600);
         return this;
       },
       events: {
-       'mouseenter .twitt': function(e) {clearTimeout(config.timedDelay);},
-       'mouseleave .twitt': "go"
+        'mouseover': "stopAnimation",
+        'mouseleave': "startAnimation"
       },
-      go: function() {
+
+      startAnimation: function(e) {
         this.twittsAnimation(config.current);
-    },
+      },
+
+      stopAnimation: function() {
+        clearTimeout(config.animationTimeout);
+      },
+
       twittsAnimation: function(val) {
 
-      var self = this, curr = (val) ? val : 0, len = self.$el.children('li').length,
-          height, $elem = self.$el.children('li').eq(curr);
+        var self = this, curr = (val) ? val : 0,
+            len = self.$el.children('li').length,
+            height,
+            $elem = self.$el.children('li').eq(curr);
 
-        if(config.fadeEffect){
-
-      $elem.animate({opacity: 0}, 600, function() {
         height = $elem.outerHeight();
-        $elem.animate({marginTop: - height}, 600);
-        if(++curr < len){
-          config.current = curr;
-          clearTimeout(config.timedDelay);
-          config.timedDelay = setTimeout(function(){self.twittsAnimation(curr)}, 900);
-        } else {
-          clearTimeout(config.timedDelay);
 
+        if (config.fadeEffect) {
+          $elem.animate({opacity: 0}, config.animationSpeed, function() {
+            $elem.animate({marginTop: -height}, config.animationSpeed);
+          })
         }
-      }) ;
-        } else {
-          height = $elem.outerHeight();
-          $elem.animate({marginTop: - height}, 600, function(){
-            if(++curr < len){
-              clearTimeout(config.timedDelay);
-              config.timedDelay = setTimeout(function(){self.twittsAnimation(curr)}, 900);
-            } else {
-              clearTimeout(config.timedDelay);
-            }
+        else {
+          $elem.animate({marginTop: -height}, config.animationSpeed, function() {
+            $elem.css({opacity: 0});
           });
         }
 
-    }
 
-  });
+        if (++curr < len) {
+          config.current = curr;
+          config.animationTimeout = setTimeout(function() {
+            self.twittsAnimation(curr)
+          }, config.animationDelay);
+        } else {
+          self.stopAnimation();
+        }
+
+
+      }
+
+    });
 
     Twitter.Views.Search = Backbone.View.extend({
       tagName: 'form',
@@ -198,18 +207,20 @@
       searchTwitts: function(e) {
         var self = this,
             value = this.$el.children("#query").val();
-        if(e.which == 13) {e.preventDefault()}
+        if (e.which === 13) {
+          e.preventDefault()
+        }
         if (value.length >= 3) {
-          if (config.timeOut) {
-            clearTimeout(config.timeOut);
+          if (config.searchTimeout) {
+            clearTimeout(config.searchTimeout);
           }
-          config.timeOut = setTimeout(function() {
+          config.searchTimeout = setTimeout(function() {
             config.search = value;
             self.collection.updateResults();
           }, config.searchDelay);
 
         } else {
-          clearTimeout(config.timeOut);
+          clearTimeout(config.searchTimeout);
         }
       },
       render: function() {
@@ -225,8 +236,8 @@
 
 
     //append search bar and search results
-    if (config.searchBar){
-    this.append(queryView.render().el);
+    if (config.searchBar) {
+      this.append(queryView.render().el);
     }
     else {
       var title = config.search,
